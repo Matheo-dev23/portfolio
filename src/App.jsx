6446b170d449f69fdc3ff254b304622e8b61e137
aquiz <matheo.paszkowski.etu@univ-lille.fr>
2025-07-08 15:36:04 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import { Header } from './components/Header'
+
+function App() {
+
+  return (
+    <div className='max-w-7xl px-4 m-auto text-white'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
